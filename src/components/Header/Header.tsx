97d34edac5a7c24820dd227485509b5600692aee
John Doe <a.xolod15@mail.ru>
2022-04-04 20:05:21 +0300

    edit(hw-36): made it a little easier

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,13 @@ const Header = () => {
             <h1 className='slider__title'>The Power of Simplicity</h1>
             <p className='slider__text'>Instead of spending time searching for the right app, our AI will bring the right app to you.</p>
             <button className='slider__btn'> <a href="/" className='slider__btn__link'> Learn </a></button>
-            <div className='slider__controls'></div>
-            <div className='slider__dot'></div>
-            <div className='slider__dot-active'></div>
+            <div className='slider__controls'>
+                <div className='slider__controls__dot'></div>
+                <div className='slider__controls__dot'></div>
+                <div className='slider__controls__dot slider__controls__dot-active'></div>
+                <div className='slider__controls__dot'></div>
+                <div className='slider__controls__dot'></div>
+            </div>
         </header>
     );
 };
