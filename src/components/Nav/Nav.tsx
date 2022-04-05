604f86cd9d71ba41a4806ce17c34171a6e2cb649
John Doe <a.xolod15@mail.ru>
2022-04-05 16:04:55 +0300

    loading logo

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,11 @@
 import './styles.scss';
+import apple from '../../assets/Apple.svg';
+
 
 const Nav = () => {
     return (
         <nav className='nav'>
-            <a href="/" className='nav__logo'><img src='../../assets/Apple.png' alt='apple' /></a>
+            <a href="/" className='nav__logo'><img src={apple} alt='apple' /></a>
             <ul className='nav__list'>
                 <li className='nav__item'><a href="/"className='nav__item--link'>Features</a></li>
                 <li className='nav__item'><a href="/"className='nav__item--link'>Partners</a></li>
